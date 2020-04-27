9625267e4a7a23f0fa075abbe487aeb7a7dbac27
Aleksandr Kurzanov <mrleksit@gmail.com>
2020-04-27 15:15:35 +0300

    lesson1

--- src/lesson1/less1.java
+++ src/lesson1/less1.java
@@ -1,4 +1,19 @@
 package lesson1;
 
-public class less1 {
-}
+import java.util.ArrayList;
+import java.util.Arrays;
+
+    public class less1 {
+
+        private static <T> void swap(T[] array, int firstIndex, int secondIndex){
+            T oneVal = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = oneVal;
+        }
+
+        private static <T> ArrayList<T> convertToList(T[] array) {
+            return new ArrayList<>(Arrays.asList(array));
+        }
+    }
+
+
