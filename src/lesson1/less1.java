1a43749928deb31731f6d49a3ee36ca5baaed3b3
Aleksandr Kurzanov <mrleksit@gmail.com>
2020-04-27 15:17:33 +0300

    lesson1

--- src/lesson1/less1.java
+++ src/lesson1/less1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
     public class less1 {
-
+        //
         private static <T> void swap(T[] array, int firstIndex, int secondIndex){
             T oneVal = array[firstIndex];
             array[firstIndex] = array[secondIndex];
